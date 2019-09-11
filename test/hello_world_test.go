a0b189ff654b0a7cd9347c48c17d832475035ebe
Neil Peterson <neilpeterson@outlook.com>
2019-09-11 13:39:12 -0700

    Initial Commit

---
+++ test/hello_world_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestHelloWorld(t *testing.T) {
+	t.Parallel()
+
+	// Generate a random DSN name to prevent a naming conflict
+	uniqueID := random.UniqueId()
+	uniqueName := fmt.Sprintf("Hello-World-%s", uniqueID)
+
+	// Specify the test case folder and "-var" options
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../terraform-config/",
+		Vars: map[string]interface{}{
+			"resource_group": uniqueName,
+			"dns-prefix":     uniqueName,
+		},
+	}
+
+	// Terraform init, apply, output, and destroy
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Get Application URL from output
+	fqdn := terraform.Output(t, terraformOptions, "fqdn")
+	query := fmt.Sprintf("http://%s", fqdn)
+
+	// Validate the provisioned application
+	http_helper.HttpGetWithCustomValidation(t, query, func(status int, content string) bool {
+		return status == 200 &&
+			strings.Contains(content, "Welcome")
+	})
+}
