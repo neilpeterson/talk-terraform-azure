3ab1db2fa7bb6e4c43961bb97727f027bcd465ac
Neil Peterson <neilpeterson@outlook.com>
2019-09-25 11:23:52 +0100

    Remove backend file

--- terraform-config/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name  = "nepetersterraformstate"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
-  }
-}
