7ec220601a722e289c99cb39e73177a1cc58f884
Neil Peterson <neilpeterson@outlook.com>
2019-09-28 11:55:05 +0200

    Updated config to .12

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "hello-world" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
+  name     = var.resource_group
+  location = var.location
 }
 
 resource "random_integer" "ri" {
@@ -13,23 +13,21 @@ resource "random_integer" "ri" {
 }
 
 resource "azurerm_container_group" "hello-world" {
-  name                = "${var.container-name}"
-  location            = "${azurerm_resource_group.hello-world.location}"
-  resource_group_name = "${azurerm_resource_group.hello-world.name}"
+  name                = var.container-name
+  location            = azurerm_resource_group.hello-world.location
+  resource_group_name = azurerm_resource_group.hello-world.name
   ip_address_type     = "public"
   dns_name_label      = "${var.dns-prefix}-${random_integer.ri.result}"
   os_type             = "linux"
 
   container {
     name   = "hello-world"
-    image  = "${var.container-image}"
+    image  = var.container-image
     cpu    = "0.5"
     memory = "1.5"
-
     ports {
       port      = 80
       protocol  = "TCP"
     }
-
   }
 }
